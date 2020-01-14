ed2821bf1a82f7bb6e590488e2f4483fee7f7c4c
keshanju <key.shan.ju@gmail.com>
2020-01-14 21:20:18 +0800

    11

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { IndexModule } from '../src/application/index/index.module';
 
-describe('AppController (e2e)', () => {
+describe('IndexController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [IndexModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
